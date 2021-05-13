0e35bf168030294034e15c27f2d7919fff8238fc
KishanItaliya <kishanp0210@gmail.com>
2021-05-13 13:36:12 +0530

    completed

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,35 +1,48 @@
-import Link from "next/link";
+import styles from "../../styles/event-item.module.css";
+import Button from "../button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
-    const { title, location, date, image, id } = props;
+  const { title, location, date, image, id } = props;
 
-    const readableDate = new Date(date).toLocaleDateString("en-Us", {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    }); 
+  const readableDate = new Date(date).toLocaleDateString("en-Us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    const formattedAddress = location.replace(", ","\n");
+  const formattedAddress = location.replace(", ", "\n");
 
-    const exploreLink = `/events/${id}`;
+  const exploreLink = `/events/${id}`;
 
-    return (
-        <li>
-            <img src={"/" +image} alt={title} />
-            <div>
-                <div>
-                    <h2>{title}</h2>
-                    <div>
-                        <time>{readableDate}</time>
-                    </div>
-                    <div>
-                        <address>{formattedAddress}</address>
-                    </div>
-                </div>
-                <Link href={exploreLink}>Explore Event</Link>
-            </div>
-        </li>
-    )
+  return (
+    <li className={styles.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={styles.content}>
+        <div className={styles.summary}>
+          <h2>{title}</h2>
+          <div className={styles.date}>
+            <DateIcon />
+            <time>{readableDate}</time>
+          </div>
+          <div className={styles.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
